3f4659fa9503b70d951d53e921f538f8545fd792
Tyler Thomas <65478430+zulumon44@users.noreply.github.com>
2021-12-05 17:17:53 -0600

    Update warning.ps1

--- warning.ps1
+++ warning.ps1
@@ -1,2 +1,7 @@
 $wshell = New-Object -ComObject Wscript.Shell
 $Output = $wshell.Popup("You Might Want to Double Check your Users & Firewall...",0,"Uh oh...", 48)
+
+# Use Below One Instead
+
+New-Item "C:\Users\DSU\Desktop\IMPORTANT.txt" -ItemType File -Value "You might want to check your Firewall and Users again..."
+Invoke-Item "C:\Users\DSU\Desktop\IMPORTANT.txt"
